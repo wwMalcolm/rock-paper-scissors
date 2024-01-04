9e70a4dd67456349e716255085a17a3b8b76943f
wwwKuroba <100085320+wwwKuroba@users.noreply.github.com>
2024-01-04 14:59:32 +0800

    Javascript code structure based on requirements

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("Hello World!");
+
+function getComputerChoice() {
+    // randomize computerchoice selection
+    let choice1 = "Rock";
+    let choice2 = "Paper";
+    let choice3 = "Scissors";
+    return computerChoice;
+}
+
+function playRound (playerSelection, computerSelection) {
+    // account for TIES
+    // case insensitive inputs from playerSelection
+    result = "You Lose! Paper beats Rock";
+    return result;
+}

4f9ef9fc8af73a367aadefe2253e30464527f2f4
wwwKuroba <100085320+wwwKuroba@users.noreply.github.com>
2024-01-04 15:46:55 +0800

    Logic for combinations of win and lose

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-console.log(getComputerChoice());
-
 function getComputerChoice() {
-    // randomy return computerchoice selection
+    // randomly return computerchoice selection
     const possibleChoices = ['Rock', 'Paper', 'Scissors'];  
     let computerChoice = possibleChoices[Math.floor(Math.random() * possibleChoices.length)];  
     return computerChoice;
@@ -10,6 +8,36 @@ function getComputerChoice() {
 function playRound (playerSelection, computerSelection) {
     // account for TIES
     // case insensitive inputs from playerSelection
-    result = "You Lose! Paper beats Rock";
+    playerSelection = playerSelection.toLowerCase();
+    let result = "";
+
+    if (playerSelection == "rock" && computerSelection == "Rock") {
+        result = "Tie! Nobody won!";
+    } else if (playerSelection == "rock" && computerSelection == "Paper") {
+        result = "You Lose! Paper beats Rock!";
+    } else if (playerSelection == "rock" && computerSelection == "Scissors") {
+        result = "You Win! Rock beats Scissors!";
+
+    } else if (playerSelection == "paper" && computerSelection == "Rock") {
+        result = "You Win! Paper beats Rock!";
+    } else if (playerSelection == "paper" && computerSelection == "Paper") {
+        result = "Tie! Nobody won!";
+    } else if (playerSelection == "paper" && computerSelection == "Scissors") {
+        result = "You Lose! Scissors beats Paper!";
+
+    } else if (playerSelection == "scissors" && computerSelection == "Rock") {
+        result = "You lose! Rock beats Scissors!";
+    } else if (playerSelection == "scissors" && computerSelection == "Paper") {
+        result = "You win! Scissors beats Paper";
+    } else if (playerSelection == "scissors" && computerSelection == "Scissors") {
+        result = "Tie! Nobody won!";
+    }
+
     return result;
 }
+
+
+const player = "rock";
+const computer = getComputerChoice();
+
+console.log(playRound(player, computer));
